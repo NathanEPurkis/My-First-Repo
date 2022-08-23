d9b18a76fe0ffb378081ca12f8ba5c621533a4dd
NathanEPurkis <nepurkis@stu.naperville203.org>
2022-08-23 14:19:39 -0500

    blueJ and notes

---
+++ My First BlueJ Project/ChapterOneNotes.java
@@ -0,0 +1,42 @@
+
+/**
+ * Nathan E. Purkis
+ * 8/23/22
+ * Chapter 1 Notes
+ * Notes from class
+ * Who helped me: Mr. Kischuk, Mom, Dog
+ */
+
+import java.util.Scanner; //class is last arg
+import java.util.Random;
+
+public class ChapterOneNotes
+{
+    /**
+     * This is code notes for chaper 1
+     * 
+     */
+    public static void main(String[] args)
+    {
+        Scanner keyboard = new Scanner(System.in);
+        
+        // basic data types
+        int x = 9,y,z;
+        boolean choice = false;
+        char letter = 'b';
+        float number2 = 9;
+        double number1 = 8.9, number3;
+        short num = 6;
+        long numbbber = 9999;
+        
+        
+        // These are objects
+        // Class id        memory    Constructor
+        String   word =    new       String("Nathan");
+        Integer  number =  new       Integer(47);
+        Double   anumber = new       Double(45.8);
+        
+        
+        System.out.print(word);
+    }
+}
